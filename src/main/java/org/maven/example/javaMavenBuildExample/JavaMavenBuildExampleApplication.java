57570141c8f45128d1c74313e354ee41ee877361
Matt Bacher <mattjb1991@sbcglobal.net>
2018-06-02 14:02:05 -0400

    Initial commit for maven build example

---
+++ src/main/java/org/maven/example/javaMavenBuildExample/JavaMavenBuildExampleApplication.java
@@ -0,0 +1,12 @@
+package org.maven.example.javaMavenBuildExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMavenBuildExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMavenBuildExampleApplication.class, args);
+	}
+}
